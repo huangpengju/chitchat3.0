f698a9f36977f96dcfeeb9a2a0f41d9f7f7ac1cc
huangpengju <15713716933@163.com>
2023-09-04 08:42:59 +0800

    主页布局

--- web/src/views/home/Hot.vue
+++ web/src/views/home/Hot.vue
@@ -1,38 +1,24 @@
 <template>
-    <el-row >
-        <el-col class="flex space-x-4" v-for="i in 3" :key="i"  :span="7" style="border: 1px solid red;">111
-                <!-- <el-space wrap >
-                    <el-card  class="box-card" style="width:300px" >
-                    <template #header>
-                        <div class="card-header">
-                        <span>Card name</span>
-                        <el-button class="button" text>Operation button</el-button>
-                        </div>
-                    </template>
-                    <div v-for="o in 4" :key="o" class="text item">
-                        {{ 'List item ' + o }}
-                    </div>
-                    </el-card>
-                </el-space> -->
+     <div class="flex flex-wrap">
+      <div class="w-1/3 p-4">
+        <div class="bg-white p-4 rounded shadow-md">
+          <h2 class="text-lg font-bold">卡片1</h2>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        </div>
+      </div>
+      <div class="w-1/3 p-4">
+        <div class="bg-white p-4 rounded shadow-md">
+          <h2 class="text-lg font-bold">卡片2</h2>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        </div>
+      </div>
+      <div class="w-1/3 p-4">
+        <div class="bg-white p-4 rounded shadow-md">
+          <h2 class="text-lg font-bold">卡片2</h2>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        </div>
+      </div>
+    </div>
 
-        </el-col>
-    </el-row>
 
   </template>
-
-<style lang="scss">
-.el-row {
-  margin-bottom: 20px;
-}
-.el-row:last-child {
-  margin-bottom: 0;
-}
-.el-col {
-  border-radius: 4px;
-}
-
-.grid-content {
-  border-radius: 4px;
-  min-height: 36px;
-}
-</style>
--- web/src/views/home/Info.vue
+++ web/src/views/home/Info.vue
@@ -1,22 +1,10 @@
 <template>
-    <el-row>
-        <el-col :span="24" style="border: 1px solid red;">1</el-col>
-    </el-row>
+   <div class="flex flex-wrap">
+      <div class="w-1/1 p-4">
+        <div class="bg-white p-4 rounded shadow-md">
+          <h2 class="text-lg font-bold">卡片1</h2>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        </div>
+      </div>
+    </div>
   </template>
-
-<style lang="scss">
-.el-row {
-  margin-bottom: 20px;
-}
-.el-row:last-child {
-  margin-bottom: 0;
-}
-.el-col {
-  border-radius: 4px;
-}
-
-.grid-content {
-  border-radius: 4px;
-  min-height: 36px;
-}
-</style>
